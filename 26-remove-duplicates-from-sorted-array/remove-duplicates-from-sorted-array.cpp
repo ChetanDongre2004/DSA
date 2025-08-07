80aea1a60a3579217d5c1d481b911279eef38a25
CHETAN DONGRE <124556820+ChetanDongre2004@users.noreply.github.com>
2025-08-07 19:10:15 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (50.65%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -5,10 +5,11 @@ public:
         int i=0;
         for(int j=1;j<n;j++)
         {
-            if(nums[j]!=nums[i])
+            if(nums[i]!=nums[j])
+            
             {
-                nums[i+1]=nums[j];
-                i++;
+               nums[i+1]=nums[j];
+               i++;
             }
 
         }
