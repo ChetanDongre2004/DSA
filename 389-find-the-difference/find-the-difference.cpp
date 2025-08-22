ca92046086c4f6772cdfe6d7fd94d7a9772faa45
CHETAN DONGRE <124556820+ChetanDongre2004@users.noreply.github.com>
2025-08-22 21:34:04 +0530

    Time: 2 ms (21.46%) | Memory: 9.5 MB (9.73%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        
+      unordered_map<char,char> mp;
+      for(int i=0;i<s.length();i++){
+        mp[s[i]]++;
+
+      }
+      for(int i=0;i<t.length();i++){
+        if(mp[t[i]]>0){
+            mp[t[i]]--;
+        }
+        else{
+            return t[i];
+        }
+        
+      }
+      return ' ';
+    }
+};
